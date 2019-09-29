55db5f33b97d0519f84f3ec471115a0b2e3f2f04
Michael Kamensky <michaelkamensky03@gmail.com>
2019-09-28 18:03:31 -0700

    Moved PCM can id to 20
    Signed-off-by: Michael Kamensky <michaelkamensky03@gmail.com>

--- src/main/java/frc/robot/HatchGrabber.java
+++ src/main/java/frc/robot/HatchGrabber.java
@@ -1,13 +1,17 @@
 package frc.robot;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Compressor;
 public class HatchGrabber {
 
     private DoubleSolenoid hatchGrabber;
     boolean grabbing = false;
+    private Compressor pneumaticsCompressor;
 
     public HatchGrabber(int pcmIndexOne, int pcmIndexTwo){
-        hatchGrabber = new DoubleSolenoid(pcmIndexOne, pcmIndexTwo); 
+        hatchGrabber = new DoubleSolenoid(20, pcmIndexOne, pcmIndexTwo); 
+        pneumaticsCompressor = new Compressor(20);
     }
+
 
     public void release() {
         hatchGrabber.set(DoubleSolenoid.Value.kReverse);
